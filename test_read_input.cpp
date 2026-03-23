522a9ce8cff5a70c4708b74804ad685b052dfee7
Generic Account 2524 <cssc2524@edoras.sdsu.edu>
2026-03-23 09:54:31 -0700

    test parsing .sic file

---
+++ test_read_input.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <string>
+#include "read_input.h"
+
+using namespace std;
+
+/*
+ * Purpose: run test and print results
+ *
+ *
+ * 
+ */
+
+void runTest (string testName, string inputLine, string expectedLabel, string expectedOpcode, string expectedOperand, bool expectedEmpty) {
+	ParseInput result = parseSicLine(inputLine);
+
+	bool passed = true;
+	
+	// Check empty flag 
+	if (result.emptyComment != expectedEmpty) {
+		cout << "FAILED: expected --> " << expectedEmpty << endl;
+		cout << "\tActual --> " << result.emptyComment << endl;
+		passed = false;
+	}	
+
+	// check other pieces
+	if (!expectedEmpty) {
+		if (result.label != expectedLabel)
+		{
+			cout << "FAILED: expected --> " << expectedLabel << endl;
+			cout << "\t Actual --> " << result.label << endl;
+			passed = false;
+		}
+		if (result.opcode != expectedOpcode){
+			cout << "FAILED: expected --> " << expectedOpcode << endl;
+			cout << "\tactual --> " << result.opcode << endl;
+			passed = false;
+
+		}
+		if (result.operand != expectedOperand) {
+
+			cout << "FAILED: expected --> " << expectedOperand << endl;
+			cout << "\tActual --> " << result.operand << endl;
+			passed = false;
+			}
+
+	}
+
+	if (passed) {cout << "PASS" << endl;}
+
+	
+}
+
+
+int main() {
+        cout << "=== Starting Input Reader Tests ===" << endl << endl;
+
+        // Test 1: Standard line with all three parts
+        //         // (Using \t to simulate tab characters from a text file)
+      
+        runTest("Test 1: Standard Line", "SUM\tSTART\t0", "SUM", "START", "0", false); 
+        // Test 2: Missing label (starts with a tab/space)                                 
+        runTest("Test 2: No Label", "\tLDA\t#0", "", "LDA", "#0", false);
+        // Test 3: Opcode only (no operand, like RSUB)
+        runTest("Test 3: No Operand", "\tRSUB", "", "RSUB", "", false);
+        // Test 4: Literal pool initialization
+        runTest("Test 4: Literal/Directive", "*\t=C'EOF'", "*", "=C'EOF'", "", false);
+        // Test 5: Completely empty string
+        runTest("Test 5: Empty Line", "", "", "", "", true);
+        // Test 6: A line containing only spaces and tabs
+        runTest("Test 6: Whitespace Only", "   \t  \r", "", "", "", true);
+	cout << "=== All Tests Completed ===" << endl;
+	return 0;
+}
